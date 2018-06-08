4bc765305cf6b0d06c5dccada7d02315ddfb5c73
Andrew Mambyk <witflash@gmail.com>
2018-06-08 15:27:49 +0300

    add public methods for ApiService

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,4 +7,29 @@ const API_URL = environment.apiUrl;
 @Injectable()
 export class ApiService {
   constructor(private http: Http) {}
+
+  // API: GET /todos
+  public getAllTodos() {
+    // this.http.get()
+  }
+
+  // API: POST /todos
+  public createTodo(todo: Todo) {
+    // this.thhp.post()
+  }
+
+  // API: GET /todos/:id
+  public getTodoById(todoId: number) {
+    // this.http.get()
+  }
+
+  // API: PUT /todos/:id
+  public updateTodo(todo: Todo) {
+    // this.http.put()
+  }
+
+  // DELETE /todos/:id
+  public deleteTodoById(todoId: number) {
+    // this.http.delete()
+  }
 }
